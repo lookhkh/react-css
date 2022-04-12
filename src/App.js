8dd0a322bca1d350af3905ab9809060fd9635fda
lookhkh <lookhkh37@gmail.com>
2022-04-12 23:16:11 +0900

    220412

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from "./component/header/Header";
 import {  BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 import Loading  from './component/header/css-practice/Loading'
 import BoxCircle from "./component/header/css-practice/BoxCircle";
+import NavPage from "./page/NavPage"
 
 
 function App() {
@@ -10,19 +11,19 @@ function App() {
  
   return (
     <Router>
-    <>
-    <nav style={{height:"5vh", display:"flex", justifyContent:"space-around", alignItems:"center", marginBottom:"15vh"}}>
-      <Link to="/">Home</Link>
-      <Link to="/loading">Loading</Link>
-      <Link to="/circle">Circle</Link>
-    </nav>
-    <div style={{height:"95vh"}}>
-      <Routes path="/" element={<App />}>
-        <Route path="loading" element={<Loading />} />
-        <Route path="circle" element={<BoxCircle />} />
-      </Routes>
-      
-    </div>
+      <>
+      <nav style={{height:"5vh", display:"flex", justifyContent:"space-around", alignItems:"center", marginBottom:"15vh"}}>
+        <Link to="/">Home</Link>
+        <Link to="/loading">Loading</Link>
+        <Link to="/circle">Circle</Link>
+      </nav>
+      <div className="root" style={{height:"95vh"}}>
+        <Routes path="/" element={<App />}>
+          <Route path="/" element={<NavPage />}/>
+          <Route path="loading" element={<Loading />} />
+          <Route path="circle" element={<BoxCircle />} />
+        </Routes>
+      </div>
     </>
     </Router>
   );
---
+++ src/page/NavPage.js
@@ -0,0 +1,95 @@
+import useLoging from '../hooks/useLoging'
+import styled  from 'styled-components'
+
+const AtagCont = ({color, name})=>{
+    return(
+        <>
+            <AtagContainer color={color}>
+                <NavItem data-name={name}>{name}</NavItem>
+            </AtagContainer>
+        </>
+    )
+}
+const NavPage = ()=>{
+
+    useLoging("NavPage");
+
+    return(
+        <Container>
+            <MainBar>
+                <AtagCont color="red" name="메일"/>
+                <AtagCont color="green" name="카페"/>
+                <AtagCont color="blue" name="지식IN"/>
+                <AtagCont color="yellow" name="블로그"/>
+            </MainBar>
+        </Container>
+    )
+}
+
+const Container = styled.div`
+
+    background-color: green;
+    width: 80%;
+    height: 80%;
+    margin: auto;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    
+`
+
+const MainBar = styled.nav`
+    width: 90%;
+    padding: 1.2rem;
+    background-color: white;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    border-radius: 0.5rem;
+
+`
+
+const AtagContainer = styled.div`
+
+    flex: 1;
+    height: 60px;
+    padding: 10px 0;
+    text-align: center;
+    overflow: hidden;
+    position: relative;
+    
+    &:hover a{
+        color: ${props=>props.color};
+        top: calc(0% - 1rem);
+    }
+  
+`
+
+const NavItem = styled.a`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+    text-align: center;
+
+
+    font-weight: 800;
+    font-size: 1.5rem;
+    transition: 0.3s linear;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translateX(-50%) translateY(-50%);
+
+
+    &::after{
+        content:attr(data-name);
+        position: absolute;
+        width: 100%;
+        left: 50%;
+        transform: translateX(-50%);
+        top: calc(100%);
+    }
+`
+
+export default NavPage;
